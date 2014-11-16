de1242dee14c50358a5dfa5d47e2fa049f40e102
Sudhi Pulla <sudheendrakumar@gmail.com>
2014-11-16 15:24:18 +1300

    Added newrelic monitoring

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['On My Game'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '5b33aa62875857cc6abf4113b67d8f2f249bd9d0',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
